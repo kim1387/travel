c59f166962abe2c7c67a5f1c46e4493f2b179c8c
kim1387 <kevink0@naver.com>
2022-11-09 10:08:54 +0900

    chore: Initial commit

---
+++ src/test/java/com/interpark/triple/TripleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interpark.triple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
