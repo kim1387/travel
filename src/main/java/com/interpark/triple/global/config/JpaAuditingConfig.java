3f70a61a8c9793ecaea65df8f372225d1ef8dbd7
kim1387 <kevink0@naver.com>
2022-11-12 16:44:45 +0900

    feat: add BaseEntity
    공통으로 사용하는 Column 모아두는 class
    - 생성 일자
    - 수정 일자
    - 활성 여부

---
+++ src/main/java/com/interpark/triple/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.interpark.triple.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
