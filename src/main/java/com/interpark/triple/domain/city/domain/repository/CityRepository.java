2345d2d96897ee27bd5ba20d965ae2bcefe6f688
kim1387 <kevink0@naver.com>
2022-11-13 03:25:10 +0900

    fix: isActivated가 true인 data만 조회하도록 메서드 변경

--- src/main/java/com/interpark/triple/domain/city/domain/repository/CityRepository.java
+++ src/main/java/com/interpark/triple/domain/city/domain/repository/CityRepository.java
@@ -2,10 +2,13 @@ package com.interpark.triple.domain.city.domain.repository;
 
 import com.interpark.triple.domain.city.domain.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CityRepository extends JpaRepository<City, Long> {
 
-    public Optional<City> findCityById(Long id);
+    @Query(value = "select c from City c where c.id = :id and c.isActivated = true")
+    Optional<City> findCityById(@Param("id") Long id);
 }
