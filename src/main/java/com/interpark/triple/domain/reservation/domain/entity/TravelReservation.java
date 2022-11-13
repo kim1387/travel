82e9e1089043ff0e17d3f26eff9ee2806c529256
kim1387 <kevink0@naver.com>
2022-11-14 01:22:42 +0900

    feat: 여행 예약정보를 관리하는 테이블 생성

---
+++ src/main/java/com/interpark/triple/domain/reservation/domain/entity/TravelReservation.java
@@ -0,0 +1,43 @@
+package com.interpark.triple.domain.reservation.domain.entity;
+
+import com.interpark.triple.domain.city.domain.entity.City;
+import com.interpark.triple.domain.user.domain.entity.Users;
+import com.interpark.triple.global.domain.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "travel_reservation")
+public class TravelReservation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", nullable = false)
+    private Users users;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @Column(name = "start_at", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(name = "end_at", nullable = false)
+    private LocalDateTime endAt;
+
+    @Builder
+    public TravelReservation(Users users, City city, LocalDateTime startAt, LocalDateTime endAt) {
+        this.users = users;
+        this.city = city;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+}
