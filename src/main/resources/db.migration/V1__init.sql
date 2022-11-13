bd37084052b33de46c75934593a652845e631982
kim1387 <kevink0@naver.com>
2022-11-14 01:23:47 +0900

    chore: flyway V1_init.sql 스크립트 작성

---
+++ src/main/resources/db.migration/V1__init.sql
@@ -0,0 +1,40 @@
+create table if not exists city
+(
+    id            bigint auto_increment
+    primary key,
+    created_date  datetime(6)  null,
+    is_activated  bit          not null,
+    updated_date  datetime(6)  null,
+    intro_content varchar(255) not null,
+    name          varchar(255) not null,
+    view          int          not null
+    );
+
+create table if not exists users
+(
+    id           bigint auto_increment
+    primary key,
+    created_date datetime(6)  null,
+    is_activated bit          not null,
+    updated_date datetime(6)  null,
+    name         varchar(255) not null,
+    role         varchar(255) not null
+    );
+
+create table if not exists travel_reservation
+(
+    id           bigint auto_increment
+    primary key,
+    created_date datetime(6) null,
+    is_activated bit         not null,
+    updated_date datetime(6) null,
+    end_at       datetime(6) not null,
+    start_at     datetime(6) not null,
+    city_id      bigint      not null,
+    users_id     bigint      not null,
+    constraint FK2id8skhl6al08h9dl0dhlxbtv
+    foreign key (users_id) references users (id),
+    constraint FKph1xcctqbrb4abh8xc4vgwk38
+    foreign key (city_id) references city (id)
+    );
+
